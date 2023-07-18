9105cb1f3f95eae3bcce47a2733eac1fef22619b
Vladimir <theing1986@gmail.com>
2023-07-18 21:28:38 +0300

    first

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -14,7 +14,7 @@ def bus_stations(request):
     with open('list.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row['first_name'], row['last_name'])
+            print(row['Number'], row['Property'], row['Title'])
 
     context = {
         #     'bus_stations': ...,
