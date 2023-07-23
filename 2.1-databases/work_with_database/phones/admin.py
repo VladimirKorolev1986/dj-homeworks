8f962329aafdbe883815da71d5475f9c1e012740
Vladimir <theing1986@gmail.com>
2023-07-23 09:53:01 +0300

    add /catalog/

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from phones.models import Phone
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'image', 'price', 'release_date', 'lte_exists'
+    prepopulated_fields = {"slug": ('name', )}
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
+
+# В файле models.py нашего приложения создаём модель Phone
+# с полями id, name, price, image, release_date, lte_exists и slug.
+# Поле id — должно быть основным ключом модели.
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50, verbose_name='name')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='price')
+    image = models.URLField(max_length=100, verbose_name='image')
+    release_date = models.DateField(verbose_name='release_date')
+    lte_exists = models.BooleanField(default=False, verbose_name='lte_exists')
+    slug = models.SlugField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
