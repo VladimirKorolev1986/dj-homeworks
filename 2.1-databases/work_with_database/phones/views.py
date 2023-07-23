a975b2a7bc1d3e565e1f9ddcc682f965e6bf54d5
Vladimir <theing1986@gmail.com>
2023-07-23 12:31:02 +0300

    Ready

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -10,11 +10,19 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     phones = Phone.objects.all()
+    if request.GET.get('sort') == 'name':
+        phones = sorted(phones, key=lambda x: x.name)
+    if request.GET.get('sort') == 'min_price':
+        phones = sorted(phones, key=lambda x: x.price)
+    if request.GET.get('sort') == 'max_price':
+        phones = sorted(phones, key=lambda x: x.price)
+        phones = phones[::-1]
     context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)[0]
+    context = {'phone': phone}
     return render(request, template, context)
