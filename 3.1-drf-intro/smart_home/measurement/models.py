89f322b201c8e047128f7c5ef36832fc366ce9ca
Vladimir <theing1986@gmail.com>
2023-08-09 21:11:38 +0300

    first

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    describe = models.CharField(max_length=200)
+
+
+class TemperatureMeasurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    temperature = models.DecimalField()
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,4 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
