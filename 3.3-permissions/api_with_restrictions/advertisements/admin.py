541e9e1d2d9087f32b3e523e41400710d8dee6c7
Vladimir <theing1986@gmail.com>
2023-08-22 21:40:14 +0300

    debug get_permission

--- 3.3-permissions/api_with_restrictions/advertisements/admin.py
+++ 3.3-permissions/api_with_restrictions/advertisements/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from advertisements.models import Advertisement
 
+
+@admin.register(Advertisement)
 class AdvertisementsAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'creator', 'created_at', 'updated_at']
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,20 +1,28 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
+from advertisements.permissions import IsOwner
 from advertisements.serializers import AdvertisementSerializer
+from django_filters import rest_framework as filters
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsOwner()]
         return []
